4357ae4a35c982e2ba292a772ff53624d46ee298
Erik <gulinianerik2@gmail.com>
2024-11-29 02:04:16 +0200

    fix footer/footer-down

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import "./App.scss";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <main className="main">
-        <Outlet />;
+        <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
